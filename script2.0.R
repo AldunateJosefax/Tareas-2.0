eb807871ee2a5b0565842d4b4a1c243115d9f2cf
AldunateJosefax <josefa.aldunate@uc.cl>
2025-11-13 18:07:50 -0300

    Initial commit

--- script2.0.R
+++ script2.0.R
@@ -4,23 +4,17 @@ install.packages("readxl")
 
 install.packages("dplyr")
 
-install.packages("httr")
 
 install.packages("jsonlite")
 
-install.packages("sf")
 
-install.packages("chilemapas")
+
 
 library(tidyverse)
 library(readxl)
 library(dplyr)
 library(ggplot2)
-library(httr)
 library(jsonlite)
-library(sf)
-library(chilemapas)
-library(ggplot2)
 
 
 datos <- read_xlsx("RNMAC al 30-09-2025 (1).xlsx")
@@ -28,20 +22,24 @@ datos <- read_xlsx("RNMAC al 30-09-2025 (1).xlsx")
 view(datos)
 colnames(datos)
 
-#usar janittor para limpiar 
 
 datos_filtrados <- datos %>% 
   group_by(Región, `Modo de obtención`) |> 
-  summarise(n = n())
+  summarise(n = n()) |> 
+  group_by(Región) |> 
+  mutate(porcentaje = n / sum(n) * 100)
   
 
 view(datos_filtrados)
 
-ggplot(datos_filtrados, aes(x = reorder(Región, n), y = n, fill = `Modo de obtención`)) +
-  geom_col(position = "dodge") +
+ggplot(datos_filtrados, aes(x = reorder(Región, porcentaje), y = porcentaje, 
+                            fill = `Modo de obtención`)) +
+  geom_col(position = "dodge2") +
+  scale_y_continuous(labels = scales::percent_format(scale = 1),
+                     limits = c(0, 100)) +
   coord_flip() +
   labs(
-    title = "Modo de obtención de perros por región - Chile (2025)",
+    title = "Perritos del Norte de Chile ¿Cómo llegan?",
     x = "Región",
     y = "Cantidad de registros",
     fill = "Modo de obtención"
@@ -53,21 +51,5 @@ ggplot(datos_filtrados, aes(x = reorder(Región, n), y = n, fill = `Modo de obte
   )
 
 
-ggplot(datos_filtrados, aes(x = `Modo de obtención`, y = n, fill = `Modo de obtención`)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ Región) +
-  labs(
-    title = "Perros del Norte - ¿Como llegan?",
-    x = "Modo de obtención",
-    y = NULL
-  ) +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.5),
-    strip.text = element_text(size = 9),
-    axis.text.y = element_blank(),       # 🔹 elimina los números del eje Y
-    axis.ticks.y = element_blank(),      # 🔹 elimina las marcas del eje Y
-    axis.text.x = element_text(angle = 45, hjust = 1)  # 🔹 gira las etiquetas para que no se monten
-  )
 
 
